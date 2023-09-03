4e32496b6191886d3663bc60871afe886348ea5f
aashish_k <aashishkathat00@gmail.com>
2023-09-04 00:49:48 +0530

    updating and deleting tour implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -52,26 +52,47 @@ exports.createTour = async (req, res) => {
         tour: newTour
       }
     });
-  } catch (err) {
+  } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: err
+      message: error
     });
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      status: 'success deleted',
+      data: null
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error
+    });
+  }
 };
