dee2dc43c24914a924804cec626c8626749a815f
aashish_k <aashishkathat00@gmail.com>
2023-09-03 20:32:56 +0530

    created connection with database and refactor code

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env'));
-console.log(process.env);
+// connect with mongoose
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// , {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+
+mongoose.connect(DB).then(con => {
+  //   console.log(con.connections);
+  console.log('database connection successfull');
+});
 
 const port = process.env.PORT || 5020;
 app.listen(port, () => {
