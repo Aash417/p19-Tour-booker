10b7ea011c2f8cec7451ddb62326f7deef044885
aashish_k <aashishkathat00@gmail.com>
2023-09-08 02:09:34 +0530

    aggregation pipeline : matching , grouping , unwinding , projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -113,3 +113,93 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingAverage: { $gt: 4.4 } }
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingQuantity' },
+          avgRating: { $avg: '$ratingAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+      //   {
+      //     $match: { _id: { $ne: 'easy' } }
+      //   }
+    ]);
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      data: {
+        stats
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numtourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: { _id: 0 }
+      },
+      {
+        $sort: { numtourStarts: -1 }
+      }
+      //   ,{ $limit: 5 }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      result: plan.length,
+      data: {
+        plan
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error
+    });
+  }
+};
