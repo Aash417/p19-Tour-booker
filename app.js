763822ab83a9885f9723012090479aa94701de40
aashish_k <aashishkathat00@gmail.com>
2023-09-09 00:04:52 +0530

    Error handling refactored

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -29,10 +31,13 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    Message: `Cant find ${req.originalUrl} on this server`
-  });
+  //   const err = new Error(`Cant find ${req.originalUrl} on this server`);
+  //   err.status = 'failllllll';
+  //   err.statusCode = 404;
+
+  next(new AppError(`Cant find ${req.originalUrl} on this server`));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
