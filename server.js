34ac70f9df324bb99f116b367ecf1cb39733ae2b
aashish_k <aashishkathat00@gmail.com>
2023-09-10 00:58:16 +0530

    Handled : unhandled rejections & unchecked exception

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+const errorController = require('./controllers/errorController');
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
 // connect with mongoose
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -16,11 +23,18 @@ const DB = process.env.DATABASE.replace(
 //   }
 
 mongoose.connect(DB).then(con => {
-  //   console.log(con.connections);
   console.log('database connection successfull');
 });
 
 const port = process.env.PORT || 5020;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
