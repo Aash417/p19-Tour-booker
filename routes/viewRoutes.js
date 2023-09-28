e0022291251ce8daba5fb5588adb8620cbf9c7b1
aashish_k <aashishkathat00@gmail.com>
2023-09-28 23:03:14 +0530

    sets up project structure

---
+++ routes/viewRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const viewsController = require('./../controllers/viewsController');
+
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   res.status(200).render('base', {
+//     tour: 'the forest hiker',
+//     user: 'ash'
+//   });
+// });
+
+router.get('/', viewsController.getOverview);
+router.get('/tour', viewsController.getTour);
+
+module.exports = router;
