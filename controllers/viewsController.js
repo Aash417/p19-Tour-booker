77de43732ea6f7bd88aa6fbb7990b2d8451b52be
aashish_k <aashishkathat00@gmail.com>
2023-09-30 18:47:57 +0530

    build tour templare with pug

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1. Get tour data from collection
@@ -14,8 +15,19 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest tiger'
+exports.getTour = catchAsync(async (req, res) => {
+  // 1. get the data for the requested tour including the tour & guide
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  // 2. build the template
+  console.log(tour);
+  // 3. Render template the using the data
+
+  res.status(200).render('tour', {
+    title: `${tour.name}`,
+    tour
+  });
+});
