a699ac00e995f59b13e8ae65f354fc8b87b8d5c5
aashish_k <aashishkathat00@gmail.com>
2023-10-06 02:52:17 +0530

    update password with our api

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,7 @@ console.log('hello from parcel(index.js)');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav_el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (loginForm)
   loginForm.addEventListener('submit', e => {
@@ -28,5 +29,26 @@ if (userDataForm)
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
 
-    updateSettings(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async e => {
+    e.preventDefault();
+
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -1,20 +1,23 @@
 /* eslint-disable */
 import axios from 'axios';
 import { showAlert } from './alerts';
-// it's either password or data
-export const updateSettings = async (name, email) => {
+
+// it's either 'password' or 'data'
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:8000/api/v1/users/updatePassword'
+        : 'http://127.0.0.1:8000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:8000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} Updated successfully`);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
