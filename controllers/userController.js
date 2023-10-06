85a970bab66eebe2d274c2cd0562617af96fa533
aashish_k <aashishkathat00@gmail.com>
2023-10-06 23:57:35 +0530

    add image to the database

--- controllers/userController.js
+++ controllers/userController.js
@@ -59,9 +59,10 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   // 2. Update user document.
 
   //   filter unwanted fields that are not allowed to be updated
-  const fileredBody = filterObj(req.body, 'name', 'email');
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
 
-  const updatedUser = await User.findByIdAndUpdate(req.user.id, fileredBody, {
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true
   });
