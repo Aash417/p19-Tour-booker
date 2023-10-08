a2f65b097fbb35d3141aafcb9cf78a4df32e49ea
aashish_k <aashishkathat00@gmail.com>
2023-10-08 11:53:38 +0530

    create new bookings on checkout

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const viewsController = require('./../controllers/viewsController');
 const authController = require('./../controllers/authController');
+const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
 // to improve performance use this only where route is not protected
 // router.use(authController.isLoggedIn);
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingsCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
